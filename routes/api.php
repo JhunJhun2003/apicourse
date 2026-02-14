34d646dc77f1b6d5f2ce2abba660482d7bef3351
kokyaw3482@gmail.com <myanmardatascience@gmail.com>
2026-02-14 23:25:27 +0630

    done api versioning

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as V1PostController;
+use App\Http\Controllers\Api\V2\PostController as V2PostController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+ 
+Route::prefix('v1')->group(function () {
+    Route::apiResource('posts', V1PostController::class);
+});     
+Route::prefix('v2')->group(function () {
+    Route::apiResource('posts', V2PostController::class);
+});     
